7575f1a2220b74af1fe6fcb9a236e8b214bd43a3
Obuli <obulikrishnaraj@gmail.com>
2024-10-04 15:07:55 -0700

    build_team_profile.py

--- build_my_team_profile.py
+++ build_my_team_profile.py
@@ -7,13 +7,14 @@ from setup_utils import get_profile_path_fine_name, run_model_team_git_parser
 
 
 def usage():
-    print("Usage: build_my_profile.py -r <repo_path> [-e <email_id>] [-n <num_years>]")
-    print("e.g. build_my_profile.py -r /home/user/repos -e user@org.ai -n 5")
+    print("Usage: build_my_team_profile.py -r <repo_path> -t <team_name> [-e <email_ids_as_csv>] [-n <num_years>]")
+    print("e.g. build_my_team_profile.py -r /home/user/repos -e user1@org.ai,user2@org.ai -t model_team -n 3")
+    print("If email_ids are not provided, profiles will be generated for all users in the repos")
     print("Default num_years is 5")
     sys.exit(1)
 
 
-def validate_input(input_path, email_id, num_years):
+def validate_input(input_path, num_years):
     """Validate the command line inputs."""
     if not os.path.isdir(input_path):
         print("Input path does not exist")
@@ -23,33 +24,23 @@ def validate_input(input_path, email_id, num_years):
         print("num_years should be a number")
         usage()
 
-    if "," in email_id:
-        print("Please provide only one email id")
-        usage()
-
 
 def main():
     parser = argparse.ArgumentParser(description="Create a ModelTeam profile.")
     parser.add_argument("-r", "--repo_path", required=True, help="Path to the repository")
-    parser.add_argument("-e", "--email_id", required=True, help="Email ID for the user")
+    parser.add_argument("-t", "--team_name", required=True, help="Name of the team")
+    parser.add_argument("-e", "--email_ids", required=False, help="Email ID for the user")
     parser.add_argument("-n", "--num_years", type=int, default=5, help="Number of years (default is 5)")
 
     args = parser.parse_args()
     input_path = args.repo_path
-    email_id = args.email_id
+    team_name = args.team_name
+    email_ids = args.email_ids
     num_years = args.num_years
 
-    validate_input(input_path, email_id, num_years)
+    validate_input(input_path, num_years)
 
-    profile_path_file = get_profile_path_fine_name()
-    if os.path.exists(profile_path_file):
-        os.remove(profile_path_file)
-
-    output_path = run_model_team_git_parser(input_path, email_id, num_years)
-
-    with open(profile_path_file, "w") as f:
-        f.write(output_path)
-
+    output_path = run_model_team_git_parser(input_path, email_ids, num_years, team_name)
     print(f"ModelTeam profile created in {output_path} directory")
 
 
--- setup_utils.py
+++ setup_utils.py
@@ -3,6 +3,7 @@ import platform
 import subprocess
 import sys
 import venv
+from datetime import datetime
 
 
 def run_command(command, shell=False):
@@ -42,3 +43,42 @@ def get_profile_path_fine_name():
     curr_dir = os.getcwd()
     profile_path_file = os.path.join(curr_dir, "model_team_profile", "model_team_profile_path.txt")
     return profile_path_file
+
+
+def sanitize_email(email):
+    """Sanitize email for use in file paths."""
+    return email.replace('@', '_').replace('.', '_')
+
+
+def run_model_team_git_parser(input_path, email_id, num_years, team_name=None, config_file="config.ini"):
+    """Run the ModelTeamGitParser script with the appropriate arguments."""
+    curr_dir = os.getcwd()
+    curr_date = datetime.now().strftime("%Y-%m-%d")
+    if team_name:
+        output_path = os.path.join(curr_dir, "model_team_profile", team_name, curr_date)
+    else:
+        # Sanitize the email ID for the output path
+        email_path = sanitize_email(email_id)
+        output_path = os.path.join(curr_dir, "model_team_profile", email_path, curr_date)
+
+    # Create the output directory
+    os.makedirs(output_path, exist_ok=True)
+    print(f"Creating ModelTeam profile in {output_path} directory")
+    python_bin = get_python_bin(create_venv=False)
+    os.environ["HF_HUB_OFFLINE"] = "1"
+    cmd = [
+        python_bin, "-m", "ModelTeamGitParser",
+        "--input_path", input_path,
+        "--output_path", output_path,
+        "--config", config_file,
+        "--user_emails", email_id,
+        "--num_years", str(num_years)
+    ]
+
+    # Check if running on macOS or Linux and add caffeinate if available
+    if sys.platform in ["darwin", "linux"]:
+        cmd = ["caffeinate"] + cmd
+
+    # Run the process
+    run_command(cmd)
+    return output_path
