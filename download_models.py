7cf86040e24fb9584eedb0cf378f0505fbe442f2
Obuli <obulikrishnaraj@gmail.com>
2024-10-07 09:09:10 -0700

    flush

--- download_models.py
+++ download_models.py
@@ -18,19 +18,18 @@ def hello_world():
 for model_type in MODEL_TYPES:
     models = get_model_list(model_team_config, model_type)
     for model_path in models:
-        print(f"Downloading {model_path} with {model_type} model type", flush=True)
         model_data = init_model(model_path, model_type, model_team_config, device)
         if model_type == C2S or model_type == LIFE_OF_PY or model_type == I2S:
             skill_list, score_list, sm_score_list = eval_llm_batch_with_scores(model_data['tokenizer'], device,
                                                                                model_data['model'], [code],
                                                                                model_data['new_tokens'], 3)
-        print(f"Downloaded {model_path} with {model_type} model type")
+        print(f"Downloaded {model_path} with {model_type} model type", flush=True)
         del model_data
         gc.collect()
-        leaks = gc.garbage
-        if leaks:
-            print("Memory leak detected! Uncollectable objects found:", flush=True)
-            for obj in leaks:
-                print(obj)
-        else:
-            print("No memory leaks detected.", flush=True)
+        # leaks = gc.garbage
+        # if leaks:
+        #     print("Memory leak detected! Uncollectable objects found:", flush=True)
+        #     for obj in leaks:
+        #         print(obj)
+        # else:
+        #     print("No memory leaks detected.", flush=True)
