04b637780e41673962c63fa7c5ddd145baa4199f
Obuli <obulikrishnaraj@gmail.com>
2024-11-04 16:32:46 -0800

    repo_dir

--- gen_team_git_stats.py
+++ gen_team_git_stats.py
@@ -28,8 +28,8 @@ def validate_input(num_years, repo_list):
 
 def main():
     parser = argparse.ArgumentParser(description="Create a ModelTeam profile.")
-    parser.add_argument("-r", "--repo_list", required=True,
-                        help="Path to the file containing paths of local git repo folders")
+    parser.add_argument("-r", "--repos", required=True,
+                        help="Path to the file containing paths of git folders or Path to directory containing git folders")
     parser.add_argument("-t", "--team_name", required=True, help="Name of the team")
     parser.add_argument("-e", "--email_ids", required=False, help="Email ID for the user")
     parser.add_argument("-n", "--num_years", type=int, default=3, help="Number of years (default is 3)")
