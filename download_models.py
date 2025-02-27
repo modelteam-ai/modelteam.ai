133cb36da9677f5772a550caf4413c4fc4c0a018
Obuli <obulikrishnaraj@gmail.com>
2025-02-27 10:34:05 -0800

    download base model

--- download_models.py
+++ download_models.py
@@ -17,7 +17,7 @@ code = """
 def hello_world():
     print("Hello World! Thanks for using ModelTeam.AI!")
 """
-print("Downloading Base Model CodeT5+...", flush=True)
+print("Downloading Base Model https://huggingface.co/Salesforce/codet5p-770m", flush=True)
 checkpoint = "Salesforce/codet5p-770m"
 tokenizer = AutoTokenizer.from_pretrained(checkpoint)
 model = AutoModelForSeq2SeqLM.from_pretrained(checkpoint).to(device)
@@ -28,7 +28,7 @@ outputs = model.generate(inputs, max_length=10)
 for model_type in MODEL_TYPES:
     models = get_model_list(model_team_config, model_type)
     for model_path in models:
-        print(f"Downloading {model_path} with {model_type} model type...", flush=True)
+        print(f"Downloading https://huggingface.co/{model_path}", flush=True)
         model_data = init_model(model_path, model_type, model_team_config, device)
         if model_type == C2S or model_type == LIFE_OF_PY or model_type == I2S:
             skill_list, score_list, sm_score_list = eval_llm_batch_with_scores(model_data['tokenizer'], device,
