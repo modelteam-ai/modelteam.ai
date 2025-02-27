472f3122d27201a61aab96dd2dc160cbfb6f62f6
Obuli Krishnaraj V <134578819+obuliv@users.noreply.github.com>
2025-02-27 10:40:07 -0800

    Split download (#57)
    * download base model
    
    * download base model

--- download_models.py
+++ download_models.py
@@ -2,6 +2,8 @@ import argparse
 import gc
 from configparser import ConfigParser
 
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+
 from modelteam_utils.constants import MODEL_TYPES, C2S, LIFE_OF_PY, I2S
 from modelteam_utils.ai_utils import eval_llm_batch_with_scores, get_model_list, init_model
 
@@ -15,11 +17,18 @@ code = """
 def hello_world():
     print("Hello World! Thanks for using ModelTeam.AI!")
 """
+print("Downloading Base Model https://huggingface.co/Salesforce/codet5p-770m", flush=True)
+checkpoint = "Salesforce/codet5p-770m"
+tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+model = AutoModelForSeq2SeqLM.from_pretrained(checkpoint).to(device)
+
+inputs = tokenizer.encode(code, return_tensors="pt").to(device)
+outputs = model.generate(inputs, max_length=10)
+
 for model_type in MODEL_TYPES:
     models = get_model_list(model_team_config, model_type)
-    print("Downloading Base Model CodeT5+...", flush=True)
     for model_path in models:
-        print(f"Downloading {model_path} with {model_type} model type...", flush=True)
+        print(f"Downloading https://huggingface.co/{model_path}", flush=True)
         model_data = init_model(model_path, model_type, model_team_config, device)
         if model_type == C2S or model_type == LIFE_OF_PY or model_type == I2S:
             skill_list, score_list, sm_score_list = eval_llm_batch_with_scores(model_data['tokenizer'], device,
