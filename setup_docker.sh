09c4edfcb92cf5d6a1abcba3aac8446566281a7a
Obuli <obulikrishnaraj@gmail.com>
2025-04-21 15:23:16 -0700

    docker

--- setup_docker.sh
+++ setup_docker.sh
@@ -12,6 +12,8 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 sudo docker run hello-world
+docker build -t modelteam-docker .
+nohup docker run -v "$PWD":/home/modelteam/app -v ~/repos:/home/mo^Clteam/repos modelteam-docker &
