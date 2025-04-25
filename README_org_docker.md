dca82d646f91a86f87a91a52bfd59dd333be208d
Obuli <obulikrishnaraj@gmail.com>
2025-04-24 22:27:22 -0700

    process team

---
+++ README_org_docker.md
@@ -0,0 +1,14 @@
+sudo apt-get install git
+git clone https://github.com/modelteam-ai/modelteam.ai.git
+Get API Keys from https://app.modelteam.ai/org/account and store them in a file called modelteam_config.txt
+clone all git repos in following structure:
+    ~/repos
+        ~/repos/team1
+            ~/repos/team1/t1-repo1
+            ~/repos/team1/t1-repo2
+        ~/repos/team2
+            ~/repos/team2/t2-repo1
+            ~/repos/team2/t2-repo2
+        ~/repos/team3
+        ....
+        ~/repos/teamN
---
+++ process_teams.py
@@ -0,0 +1,97 @@
+import argparse
+import datetime
+import os
+import re
+import sys
+
+import requests
+
+from modelteam.modelteam_utils.constants import MT_PROFILE_JSON
+from setup_utils import run_model_team_git_parser
+
+
+def usage():
+    print("Usage: process_teams.py -r <repo_dir> -t <team_name> [-n <num_years>]")
+    print("e.g. process_teams.py -r /home/user/repos -t model_team -n 3")
+    print("Default num_years is 3")
+    sys.exit(1)
+
+
+def validate_input(num_years, repo_list, config):
+    """Validate the command line inputs."""
+    if repo_list and not os.path.isdir(repo_list):
+        print("Repo list does not exist")
+        usage()
+
+    if not re.match(r"^[0-9]+$", str(num_years)):
+        print("num_years should be a number")
+        usage()
+
+    if not os.path.exists(config):
+        print("Config file does not exist")
+        usage()
+
+
+def load_config(config):
+    """Load the configuration file and return the org_hash and api_key."""
+    org_hash = None
+    api_key = None
+    with open(config, 'r') as f:
+        for line in f:
+            if line.startswith("org_id"):
+                org_hash = line.split("=")[1].strip()
+            elif line.startswith("api_key"):
+                api_key = line.split("=")[1].strip()
+    return org_hash, api_key
+
+
+def upload_profile(org_hash, api_key, merged_json):
+    url = "https://yourdomain.com/team"
+    headers = {
+        "Authorization": f"Bearer {api_key}"
+    }
+    data = {
+        "org_hash": org_hash
+    }
+    files = {
+        "file": open(merged_json, "rb")
+    }
+    response = requests.put(url, headers=headers, data=data, files=files)
+    if response.status_code == 200:
+        print("Profile uploaded successfully.")
+    else:
+        print("Failed to upload profile.")
+        print("Response Code:", response.status_code)
+        print("Response:", response.json())
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Create a modelteam profile.")
+    parser.add_argument("-r", "--repos", required=True,
+                        help="Path to directory containing git folders")
+    parser.add_argument("-n", "--num_years", type=int, default=3, help="Number of years (default is 3)")
+    parser.add_argument("-c", "--config", required=True)
+
+    args = parser.parse_args()
+    repo_list = args.repos
+    num_years = args.num_years
+    config = args.config
+    org_hash, api_key = load_config(config)
+    validate_input(num_years, repo_list, config)
+    teams = os.listdir(repo_list)
+    filtered_teams = [team for team in teams if os.path.isdir(os.path.join(repo_list, team))]
+    if len(filtered_teams) == 0:
+        print("No git folders found in the directory")
+        usage()
+    for team in filtered_teams:
+        team_path = os.path.join(repo_list, team)
+        output_path = run_model_team_git_parser(team_path, None, num_years, False, team)
+        end_ts = int(datetime.datetime.now(datetime.timezone.utc).timestamp())
+        end_date = datetime.datetime.fromtimestamp(end_ts, tz=datetime.timezone.utc).strftime('%Y-%m-%d')
+        merged_json = os.path.join(output_path, MT_PROFILE_JSON)
+        merged_json = f"{merged_json}_{end_date}.gz"
+        upload_profile(org_hash, api_key, merged_json)
+
+
+if __name__ == "__main__":
+    main()
